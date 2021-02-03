cab6cca79926ef8369dc7b2ce9a64e44b0becf8f
Obanda <emmanuelwakhisi@gmail.com>
2021-02-03 05:32:35 +0300

    Markdown support to the Blog post

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -18,12 +18,12 @@
                         <div class="post-item-body">
                             <div class="padding-10">
                                 <h2><a href="{{ route('blog.show', $post->slug) }}">{{ $post->title }}</a></h2>
-                                <p>{{ $post->excerpt }}</p>
+                                {!! $post->excerpt_html !!}
                             </div>
                             <div class="post-meta padding-10 clearfix">
                                 <div class="pull-left">
                                     <ul class="post-meta-group">
-                                        <li><i class="fa fa-user"></i><a href="#">{{ $post->author->name }}</a></li>
+                                        <li><i class="fa fa-user"></i><a href="#">!!{ $post->author->name }}</a></li>
                                         <li><i class="fa fa-clock-o"></i><time>{{ $post->date }}</time></li>
                                         <li><i class="fa fa-tags"></i><a href="#"> Blog</a></li>
                                         <li><i class="fa fa-comments"></i><a href="#">4 Comments</a></li>
