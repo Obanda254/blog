aa9490bcc9befa27a1842965590aa85ff26b634f
Obanda <emmanuelwakhisi@gmail.com>
2021-02-04 09:58:30 +0300

    Display categories in archive cards

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -13,31 +13,28 @@ class BlogController extends Controller
         
     public function index()
     {
-        $categories = Category::with(['posts' => function($query) {
-            $query->published();
-        }])->orderBy('title', 'asc')->get();
-
         $posts = Post::with('author')
                     ->latestFirst()
                     ->published()
                     ->simplePaginate($this->limit);
 
-        return view('blog.index', compact('posts', 'categories'));
+        return view('blog.index', compact('posts'));
     }
 
-    public function category($id)
+    public function category(Category $category)
     {
-        $categories = Category::with(['posts' => function($query) {
-            $query->published();
-        }])->orderBy('title', 'asc')->get();
+        $categoryName = $category->title;
 
-        $posts = Post::with('author')
-                    ->latestFirst()
-                    ->published()
-                    ->where('category_id', $id)
-                    ->simplePaginate($this->limit);
+        // \DB::enableQueryLog();
+        $posts = $category->posts()
+                          ->with('author')
+                          ->latestFirst()
+                          ->published()
+                          ->simplePaginate($this->limit);
 
-        return view('blog.index', compact('posts', 'categories'));
+        return view('blog.index', compact('posts', 'categoryName'));
+
+        // dd(\DB::getQueryLog());
     }
 
     public function show(Post $post)
